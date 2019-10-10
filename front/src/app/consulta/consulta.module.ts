b91d91c1e5e017bdd0ebdea0f32c2a2f409d5205
WellingtonFidelis <wellingtonf20@gmail.com>
2019-10-10 13:14:58 -0300

    melhorando o front de consulta detalhe

--- front/src/app/consulta/consulta.module.ts
+++ front/src/app/consulta/consulta.module.ts
@@ -1,20 +1,20 @@
-import{ NgModule } from '@angular/core';
-import { CommonModule } from "@angular/common";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { JwPaginationComponent } from 'jw-angular-pagination';
 import { FilterPipe } from './formulario-consulta/filter.pipe';
 import { FormsModule } from '@angular/forms';
 
-import { FormularioConsultaComponent } from "src/app/consulta/formulario-consulta/formulario.consulta.component";
-import { FormularioNaoEncontradoComponent } from "src/app/consulta/formulario-nao-encontrado/formulario.nao.encontrado.component";
-import { ConsultaRoutingModule } from "src/app/consulta/consulta.routing.module";
-import { FormularioConsultaDetalheComponent } from "src/app/consulta/formulario-consulta-detalhe/formulario.consulta.detalhe.component";
+import { FormularioConsultaComponent } from 'src/app/consulta/formulario-consulta/formulario.consulta.component';
+import { FormularioNaoEncontradoComponent } from 'src/app/consulta/formulario-nao-encontrado/formulario.nao.encontrado.component';
+import { ConsultaRoutingModule } from 'src/app/consulta/consulta.routing.module';
+import { FormularioConsultaDetalheComponent } from 'src/app/consulta/formulario-consulta-detalhe/formulario.consulta.detalhe.component';
 
 @NgModule({
 
     imports: [
         CommonModule,
         ConsultaRoutingModule,
-        FormsModule      
+        FormsModule
     ],
 
     declarations: [
@@ -26,7 +26,7 @@ import { FormularioConsultaDetalheComponent } from "src/app/consulta/formulario-
     ],
 
     providers: [
-        
+
     ]
 
 })
