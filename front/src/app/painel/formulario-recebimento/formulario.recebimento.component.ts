e0c1ad2110938362b2f1c700cae39a66ee56c363
WellingtonFidelis <wellingtonf20@gmail.com>
2020-01-08 12:07:27 -0300

    Adicionando estilo no Alert quando não salva as informações do formulário

--- front/src/app/painel/formulario-recebimento/formulario.recebimento.component.ts
+++ front/src/app/painel/formulario-recebimento/formulario.recebimento.component.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import { IFormularioCanDeactivate } from 'src/app/guards/iformulario.candeactivate';
@@ -11,57 +12,58 @@ export class FormularioRecebimentoComponent implements OnInit, IFormularioCanDea
 
   mostraMensagemSucesso: boolean;
   mostraMensagemErro: boolean;
-  private formularioMudou: boolean = false;
+  private formularioMudou = false;
 
   constructor(private http: Http) { }
 
   ngOnInit() {
-    
+
    }
 
-  enviar(formulario){
+  enviar(formulario) {
 
-    //envia pelo método post um json com os valores informado nos campos do formulário
+    // envia pelo método post um json com os valores informado nos campos do formulário
     this.http.post('painel', formulario.value)
       .subscribe(dados => {
-      //apaga as informações dos campos do formulário quando o mesmo é enviado com sucesso
+      // apaga as informações dos campos do formulário quando o mesmo é enviado com sucesso
       formulario.reset();
       this.mostraMensagemSucesso = true;
-      //mensagem de sucesso é exibida por 5 segundos
+      // mensagem de sucesso é exibida por 5 segundos
       setTimeout(() => this.mostraMensagemSucesso = false, 5000);
     },
 
       (error: any) => (this.mostraMensagemErro = true)
-      
+
       );
 
-    //mensagem de erro por qualquer motivo é exibida por 5 segundos
+    // mensagem de erro por qualquer motivo é exibida por 5 segundos
     setTimeout(() => this.mostraMensagemErro = false, 5000);
     }
 
 
-    //verifica se ouve alguma mudança em algum campo do formulário
-  onInput(){
+    // verifica se ouve alguma mudança em algum campo do formulário
+  onInput() {
 
     this.formularioMudou = true;
 
   }
 
-    //verificação se pode ou não mudar de página enquanto altera informações
+    // verificação se pode ou não mudar de página enquanto altera informações
   podeMudarRota() {
-      
-    //se o formulário sofreu alguma altração nos campos e o usuário tenta mudar de página
-    if (this.formularioMudou){
 
-    //é mostrada um aviso se deseja continuar ou cancelar
-    var caixaDialogo = confirm("Tem certeza que deseja sair dessa página? " + 
-      "As modificações não serão salvas!");
-      
-      //se o usuário clicar em ok, é perdido as informações alteradas e redecionado para nova página
-      if(caixaDialogo == true) return true;
-      //se o usuário clicar em cancelar, as informações não são perdidas e o usuário continua na mesma página
-      else return false ;
-  
+    // se o formulário sofreu alguma altração nos campos e o usuário tenta mudar de página
+    if (this.formularioMudou) {
+
+    // é mostrada um aviso se deseja continuar ou cancelar
+    // tslint:disable-next-line: prefer-const
+    let caixaDialogo = confirm('Tem certeza que deseja sair dessa página?' +
+      'As modificações não serão salvas!');
+
+      // se o usuário clicar em ok, é perdido as informações alteradas e redirecionado para nova página
+      if (caixaDialogo === true)  return true;
+      // se o usuário clicar em cancelar, as informações não são perdidas e o usuário continua na mesma página
+      else return false;
+
     }
 
     return true;
